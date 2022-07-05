f3d98376c377a54d431e6ee6ac2d7fd00fc96d2a
jcmontano <108704333+jcmontano@users.noreply.github.com>
2022-07-05 09:08:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Breakfast Menu')
-
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 
 streamlit.text('Kale, Spinach & Rocket Smoothie')
